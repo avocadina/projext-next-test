298f17fabfa937b34c72f785d4d2d8cfb54c58e9
leSchb <kurultae@yandex.ru>
2025-02-06 14:44:14 +0300

    initial

--- test-app/next.config.ts
+++ test-app/next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
-
+const path = require("path");
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: false,
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ["@svgr/webpack"],
+		});
+
+		return config;
+	},
+
+	sassOptions: {
+		includePaths: [path.join(__dirname, "src")],
+		prependData: `
+        @import "app/styles/global/colors.scss";
+        @import "app/styles/global/sizes.scss";
+        @import "app/styles/global/mixins.scss";
+        `,
+	},
+
+	compiler: {
+		// Enables the styled-components SWC transform
+		styledComponents: true,
+	},
+	output: "standalone",
 };
 
 export default nextConfig;
---
+++ test-app/src/app/client-layout/ui/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import classes from "./client-layout.module.css";
+
+export default function ClientLayout() {
+    return (
+        <div className="grid">
+            
+        </div>
+    )
+}
